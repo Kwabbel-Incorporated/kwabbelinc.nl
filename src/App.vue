bddd5378afc914d26414b36fa91036f450ef331b
Agent-Kwabbel <FabeDGStuff@hotmail.com>
2023-09-07 18:30:02 +0200

    Font display swap toegevoegd

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ export default {
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200..900');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200..900&display=swap');
 
 body {
   margin: 0px;
